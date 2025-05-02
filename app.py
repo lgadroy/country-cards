e05f037a5434349f08f738dbb47664eea52536f9
lgadroy <leo.gadroy@gmail.com>
2025-05-02 18:04:33 +0200

    Adding flags for language switching.

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def home():
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="127.0.0.1", port=8080)
+    #serve(app, host="0.0.0.0", port=8080)
